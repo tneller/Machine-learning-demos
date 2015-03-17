8c581c34c1b5091e05fed16b9e79e9045755ef80
zuozhiyang <yangzuozhii@163.com>
2015-03-16 21:53:51 -0400

    Update Clusters.java

--- src/kmeans/Clusters.java
+++ src/kmeans/Clusters.java
@@ -1,26 +1,34 @@
 import java.util.LinkedList;
-
+/**
+ * Cluster class represent iterations in K-means
+ */
 public class Clusters 
 {
-	double innerDist;
+	double innerDist;//within-cluster sum of square
 	LinkedList<Point>[] clusters;
-	Point[] centroids;
-	public double getInnerDist() {
+	Point[] centroids;//array of centroids
+	public double getInnerDist() 
+	{
 		return innerDist;
 	}
-	public void setInnerDist(double innerDist) {
+	public void setInnerDist(double innerDist) 
+	{
 		this.innerDist = innerDist;
 	}
-	public LinkedList<Point>[] getClusters() {
+	public LinkedList<Point>[] getClusters() 
+	{
 		return clusters;
 	}
-	public void setClusters(LinkedList<Point>[] clusters) {
+	public void setClusters(LinkedList<Point>[] clusters) 
+	{
 		this.clusters = clusters;
 	}
-	public Point[] getCentroids() {
+	public Point[] getCentroids() 
+	{
 		return centroids;
 	}
-	public void setCentroids(Point[] centroids) {
+	public void setCentroids(Point[] centroids) 
+	{
 		this.centroids = centroids;
 	}
 }
