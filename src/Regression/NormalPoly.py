7ddacf3732394064d32a8cbea0503c9369cebf15
zuozhiyang <yangzuozhii@163.com>
2015-03-24 15:23:33 -0400

    Update NormalPoly.py

--- src/Regression/NormalPoly.py
+++ src/Regression/NormalPoly.py
@@ -1,9 +1,7 @@
 import pylab as pl
 import numpy as np
 
-
-
-origin = [50,0,-0.7]
+origin = [50,0,-0.7] # original equation for data generating
 
 def computeY(x,beta):
     y = 0.0
@@ -11,10 +9,8 @@ def computeY(x,beta):
         y += beta[i]*np.power(x,i)
     return y
 
-
-
 degree = 1
-numOfData = 10
+numOfData = 10 # number of datasets
 
 pl.figure(figsize=(16, 8))
 pl.subplots_adjust(left=.001, right=.999, bottom=.001, top=.999, wspace=.001,hspace=.1)
@@ -25,8 +21,6 @@ for x in r:
     s = np.random.normal(loc = computeY(x,origin),scale = 1.75)
     sample.append(s)
 for index in range(1,7):
-    
-    
     
     pl.subplot(2,3,index)
     pl.plot(r,sample,'ro')
@@ -41,13 +35,13 @@ for index in range(1,7):
     
     I = np.identity(index+1)
     I[0][0] = 0
-    Beta = np.linalg.inv(X.transpose().dot(X)+I).dot(X.transpose()).dot(sample)
+    Beta = np.linalg.inv(X.transpose().dot(X)+I).dot(X.transpose()).dot(sample) # calculate regularized parameter vector
     Beta = np.squeeze(np.asarray(Beta))
-    print Beta
+    print Beta # print parameters
  
     pl.axis([0,numOfData+1,0,50])
-    pl.plot(r,computeY(r,origin),"b-")
-    pl.plot(r,computeY(r,Beta),"r-")
+    pl.plot(r,computeY(r,origin),"b-") #plot original equation
+    pl.plot(r,computeY(r,Beta),"r-") #plot prodicting equation
     degree += 1
 
 pl.show()
